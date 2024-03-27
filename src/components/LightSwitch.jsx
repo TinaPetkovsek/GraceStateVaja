8d585ad6f8a2013a0b0892bd5513075aad77d406
tinapetkovsek <tina1.petkovsek@gmail.com>
2024-03-27 18:03:39 +0100

    dodala stvari

--- src/components/LightSwitch.jsx
+++ src/components/LightSwitch.jsx
@@ -3,9 +3,21 @@ import { Switch } from "./ui/switch";
 
 export function LightSwitch() {
   // Naredi state `isDark`
+  const [isDark, setIsDark] = useState(false);
 
   // Uporabi useEffect za dodajanje ali odstranjevanje classa `dark` na `document.documentElement` glede na vrednost `isDark`
 
+  useEffect(() => {
+    // To kodo daj v useEffect
+    const root = document.documentElement; // najde <html> značko
+    root.classList.remove("light", "dark"); // odstrani prejšnji class
+    root.classList.add(isDark ? "dark" : "light"); // dodaj nov class glede na vrednost isDark
+  }, [isDark]);
+
   // Prikazi Switch komponento in ji posreduj `isDark` in `setIsDark` kot props
-  return <div>To je LightSwitch.jsx</div>;
+  return (
+    <Switch checked={isDark} onCheckedChange={setIsDark}>
+      To je LightSwitch.jsx
+    </Switch>
+  );
 }
--- src/components/TextLengthCounter.jsx
+++ src/components/TextLengthCounter.jsx
@@ -1,5 +1,37 @@
 import { useEffect, useState } from "react";
+import { Textarea } from "./ui/textarea";
+import { Slider } from "./ui/slider";
 
 export function TextLengthCounter() {
-  return <div>To je TextLengthCounter.jsx</div>;
+  const [text, setText] = useState("");
+  const [maxChars, setMaxChars] = useState(100);
+  const [isOverLimit, setIsOverlimit] = useState(false);
+
+  // useEffect(()=>{},)
+
+  return (
+    <div>
+      <Textarea
+        value={text}
+        onChange={(v) => setText(v.target.value)}
+      ></Textarea>
+      <div></div>
+      <Slider
+        value={[maxChars]}
+        onValueChange={(a) => setMaxChars(a[0])}
+        defaultValue={[33]}
+        max={100}
+        step={1}
+      />
+      <div>{maxChars}</div>
+
+      {text.length > maxChars ? (
+        <div>Tvoje besedilo je predolgo</div>
+      ) : (
+        <p>b</p>
+      )}
+
+      {text.length > maxChars && <div>Tvoje besedilo je predolgo</div>}
+    </div>
+  );
 }
